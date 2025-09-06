70966499f763a0ad7aeae280e981322b8d187b1e
Ameya Bhalerao <52749271+ab39912@users.noreply.github.com>
2025-09-06 15:01:16 +0000

    updated streamlit_app

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,8 +5,8 @@ st.set_page_config(page_title="HW-Manager", layout="wide")
 
 nav = st.navigation({
     "Labs": [
-        st.Page(lab1.run, title="HW 1", url_path="hw1"),
-        st.Page(lab2.run, title="HW 2 (Default)", url_path="hw2", default=True),
+        st.Page(hw1.run, title="HW 1", url_path="hw1"),
+        st.Page(hw2.run, title="HW 2 (Default)", url_path="hw2", default=True),
     ]
 })
 
