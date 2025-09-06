01463e5a1e5578fa797e8bb09cb0b59322ee6c41
Ameya Bhalerao <52749271+ab39912@users.noreply.github.com>
2025-09-06 15:35:28 +0000

    changed title

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,9 +4,9 @@ import lab1, lab2
 st.set_page_config(page_title="Document QA", layout="wide")
 
 nav = st.navigation({
-    "Labs": [
-        st.Page(lab1.run, title="Lab 1", url_path="lab-1"),
-        st.Page(lab2.run, title="Lab 2 (Default)", url_path="lab-2", default=True),
+    "HW": [
+        st.Page(lab1.run, title="HW 1", url_path="lab-1"),
+        st.Page(lab2.run, title="HW 2 (Default)", url_path="lab-2", default=True),
     ]
 })
 
